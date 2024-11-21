fd3f19d73da6fe653ad58b653b093c625a1f64af
Jay Hogan <jayhogan.dev@gmail.com>
2024-11-21 18:46:48 -0500

    theme context added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,14 @@
 import { View, Text } from 'react-native';
 import React from 'react';
+import { Stack } from 'expo-router';
+import { ThemeProvider } from '../context/ThemeContext';
 
 export default function _layout() {
   return (
-    <View>
-      <Text>_layout</Text>
-    </View>
+    <ThemeProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+      </Stack>
+    </ThemeProvider>
   );
 }
